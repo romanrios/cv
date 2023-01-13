e7f35df29f3b522c379273fd45ddfc6b64245da0
Roman Rios <romanrios@live.com>
2023-01-12 21:52:39 -0300

    barra navegacion

---
+++ function.js
@@ -0,0 +1,4 @@
+document.getElementById("boton").onclick = function () {
+    console.log("Capturamos el evento click");
+    document.getElementById("hhero").innerHTML = "Esto es una prueba de JavaScript";
+}
