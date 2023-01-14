aa1460722e43a6fe5f451efefaf684ca174c641f
Roman Rios <romanrios@live.com>
2023-01-14 15:27:34 -0300

    idioma y footer

--- function.js
+++ function.js
@@ -1,4 +1,35 @@
-document.getElementById("boton").onclick = function () {
-    console.log("Capturamos el evento click");
-    document.getElementById("hhero").innerHTML = "Esto es una prueba de JavaScript";
+document.getElementById("en").onclick = function () {
+    document.getElementById("hhero").innerHTML = "Graphic designer & developer"
+    document.getElementById("nav_inicio").innerHTML = "Home";
+    document.getElementById("nav_proyectos").innerHTML = "Projects";
+    document.getElementById("nav_contacto").innerHTML = "Contact";
+    document.getElementById("title_proyectos").innerHTML = "Fulfilled projects"; 
+    document.getElementById("title_contacto").innerHTML = "Contact me"; 
+    document.getElementById("link1").innerHTML = "Link to project";
+    document.getElementById("link2").innerHTML = "Link to project";
+    document.getElementById("link3").innerHTML = "Link to project";
+    document.getElementById("proyecto_futumag").innerHTML = "Digital magazine about design and technology"; 
+    document.getElementById("proyecto_wesound").innerHTML = "Social network for collaborative music production"; 
+    document.getElementById("proyecto_pirogram").innerHTML = "Visual identity for design studio"; 
+    document.getElementById("boton_enviar").value = "Send";
+    document.getElementById("form_nombre").placeholder = "Name:";
+    document.getElementById("form_mensaje").placeholder = "Message:";
+}
+
+document.getElementById("es").onclick = function () {
+    document.getElementById("hhero").innerHTML = "Diseñador gráfico y programador";
+    document.getElementById("nav_inicio").innerHTML = "Inicio";
+    document.getElementById("nav_proyectos").innerHTML = "Proyectos";
+    document.getElementById("nav_contacto").innerHTML = "Contacto";
+    document.getElementById("title_proyectos").innerHTML = "Proyectos realizados"; 
+    document.getElementById("title_contacto").innerHTML = "Contacta conmigo"; 
+    document.getElementById("link1").innerHTML = "Enlace al proyecto";
+    document.getElementById("link2").innerHTML = "Enlace al proyecto";
+    document.getElementById("link3").innerHTML = "Enlace al proyecto";
+    document.getElementById("proyecto_futumag").innerHTML = "Magazine digital sobre diseño y tecnología"; 
+    document.getElementById("proyecto_wesound").innerHTML = "Red social de producción musical colaborativa"; 
+    document.getElementById("proyecto_pirogram").innerHTML = "Identidad visual para estudio de diseño";
+    document.getElementById("boton_enviar").value = "Enviar";
+    document.getElementById("form_nombre").placeholder = "Nombre:";
+    document.getElementById("form_mensaje").placeholder = "Mensaje:";
 }
