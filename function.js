714ba00c6687edc3320c7c69920e230fee1a4267
Roman Rios <romanrios@live.com>
2023-06-28 16:12:36 -0300

    habilidades. traducciones.

--- function.js
+++ function.js
@@ -24,18 +24,27 @@ document.getElementById("en").onclick = function () {
     /*habilidades*/
     document.getElementById("hab_desarrollo").innerHTML = "DEVELOPMENT";
     document.getElementById("hab_diseño").innerHTML = "DESIGN";
+    document.getElementById("hab_musica").innerHTML = "MUSIC";
     document.getElementById("hab_identidadvisual").innerHTML = "Visual identity";
     document.getElementById("hab_diseñowebyapps").innerHTML = "Web design & apps";
     document.getElementById("hab_interfazdeusuario").innerHTML = "User interface";
     document.getElementById("hab_diseñodeinformacion").innerHTML = "Infomation design";
     document.getElementById("hab_diseñoeditorial").innerHTML = "Editorial design";
+    document.getElementById("hab_musicaoriginal").innerHTML = "Original music";
+    document.getElementById("hab_efectosdesonido").innerHTML = "Sound effects";
+    document.getElementById("hab_sintetizadores").innerHTML = "Synthesizers";
+    document.getElementById("hab_guitarra").innerHTML = "Guitar";
+    document.getElementById("hab_vst").innerHTML = "VST Virtual instruments";
     /*proyectos*/
     document.getElementById("link_a").innerHTML = "> Link to project&nbsp;";
     document.getElementById("link_b").innerHTML = "> Link to project&nbsp;";
     document.getElementById("link_c").innerHTML = "> Link to project&nbsp;";
+    document.getElementById("link_d").innerHTML = "> Link to project&nbsp;";
     document.getElementById("proyecto_futumag").innerHTML = "Digital magazine about design and technology";
     document.getElementById("proyecto_wesound").innerHTML = "Social network for collab music production";
     document.getElementById("proyecto_pirogram").innerHTML = "Visual identity for design studio";
+    document.getElementById("proyecto_rpggame").innerHTML = "JavaScript based RPG game";
+    document.getElementById("proyecto_musicalexperiments").innerHTML = "Collection of original compositions "; 
     /*contacto*/
     document.getElementById("boton_enviar").value = "Send";
     document.getElementById("form_nombre").placeholder = "Name:";
@@ -68,19 +77,28 @@ document.getElementById("es").onclick = function () {
     /*habilidades*/
     document.getElementById("hab_desarrollo").innerHTML = "DESARROLLO";
     document.getElementById("hab_diseño").innerHTML = "DISEÑO";
+    document.getElementById("hab_musica").innerHTML = "MÚSICA";
     document.getElementById("hab_identidadvisual").innerHTML = "Identidad visual";
     document.getElementById("hab_diseñowebyapps").innerHTML = "Diseño web y apps";
     document.getElementById("hab_interfazdeusuario").innerHTML = "Interfaz de usuario";
     document.getElementById("hab_diseñodeinformacion").innerHTML = "Diseño de información";
     document.getElementById("hab_diseñoeditorial").innerHTML = "Diseño editorial";
+    document.getElementById("hab_musicaoriginal").innerHTML = "Música original";
+    document.getElementById("hab_efectosdesonido").innerHTML = "Efectos de sonido";
+    document.getElementById("hab_sintetizadores").innerHTML = "Sintetizadores";
+    document.getElementById("hab_guitarra").innerHTML = "Guitarra";
+    document.getElementById("hab_vst").innerHTML = "Instrumentos virtuales VST";
     /*proyectos*/
     document.getElementById("link_a").innerHTML = "> Enlace al proyecto&nbsp;";
     document.getElementById("link_b").innerHTML = "> Enlace al proyecto&nbsp;";
     document.getElementById("link_c").innerHTML = "> Enlace al proyecto&nbsp;";
+    document.getElementById("link_d").innerHTML = "> Enlace al proyecto&nbsp;";
     document.getElementsByClassName("link").innerHTML = "Enlace al proyecto";
     document.getElementById("proyecto_futumag").innerHTML = "Magazine digital sobre diseño y tecnología";
     document.getElementById("proyecto_wesound").innerHTML = "Red social de producción musical colaborativa";
     document.getElementById("proyecto_pirogram").innerHTML = "Identidad visual para estudio de diseño";
+    document.getElementById("proyecto_rpggame").innerHTML = "Juego estilo RPG basado en JavaScript";  
+    document.getElementById("proyecto_musicalexperiments").innerHTML = "Colección de composiciones originales"; 
     /*contacto*/
     document.getElementById("boton_enviar").value = "Enviar";
     document.getElementById("form_nombre").placeholder = "Nombre:";
