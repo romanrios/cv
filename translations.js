2c3a74dd9a66741aea41ca4539e0f54d7f3a4ddf
Román Ríos <romanrios@live.com>
2025-02-03 14:05:51 -0300

    translations.js / visual designer

---
+++ translations.js
@@ -0,0 +1,272 @@
+export const elements_innerHTML = {
+  hero_subtitle: {
+    en: "Software Developer /<br/>Visual Designer",
+    es: "Desarrollador de Software /<br/>Diseñador de Comunicación Visual",
+  },
+  hero2_text: {
+    en: "Hello, I'm Román!<br /><br />👨‍💻 Software Developer and Visual Designer with a specialization in Web Development and Multimedia Design.<br /><br />🎓 I completed my degree at FADU of the National University of Litoral, with supplementary studies in Web Development and Programming. I am currently pursuing a Higher Technical Degree in Software Development, strengthening my professional profile.<br /><br />💼 I work as an intern at the Ministry of Labor, Employment, and Social Security of the Province of Santa Fe, and also lead projects as a freelance professional in the area of Game Design and Development.<br /><br />⚙️ My professional approach is based on proactivity, teamwork, effective communication, and adaptability, with a constant commitment to training to face challenges and achieve goals.<br /><br />🚀 Ready to bring all my potential and experience to new projects. Let's connect!",
+    es: "¡Hola, soy Román!<br /><br />👨‍💻 Desarrollador de Software y Diseñador de Comunicación Visual con especialización en Desarrollo Web y Diseño Multimedia.<br /><br />🎓 Realicé mi carrera en la FADU de la Universidad Nacional del Litoral, con estudios complementarios en Desarrollo Web y Programación. Actualmente cursando la Tecnicatura Superior en Desarrollo de Software, fortaleciendo mi perfil profesional.<br /><br />💼 Me desempeño como pasante en el Ministerio de Trabajo, Empleo y Seguridad Social de la Provincia de Santa Fe, y además llevo adelante proyectos como profesional independiente en el área de Diseño y Desarrollo de Videojuegos.<br /><br />⚙️ Mi enfoque profesional se basa en la proactividad, el trabajo en equipo, la comunicación efectiva y la adaptabilidad, con un compromiso constante hacia la formación para enfrentar desafíos y alcanzar objetivos.<br /><br />🚀 Listo para aportar todo mi potencial y experiencia a nuevos proyectos. ¡Conectemos!",
+  },
+  nav_inicio: {
+    en: "Home",
+    es: "Inicio",
+  },
+  nav_experiencia: {
+    en: "Experience",
+    es: "Experiencia",
+  },
+  nav_habilidades: {
+    en: "Skills",
+    es: "Habilidades",
+  },
+  nav_contacto: {
+    en: "Contact",
+    es: "Contacto",
+  },
+  nav_downloads: {
+    en: "Downloads",
+    es: "Descargas",
+  },
+  title_experiencia: {
+    en: "Experience",
+    es: "Experiencia",
+  },
+  title_habilidades: {
+    en: "Skills",
+    es: "Habilidades",
+  },
+  title_datos: {
+    en: "Contact",
+    es: "Contacto",
+  },
+  title_downloads: {
+    en: "Downloads",
+    es: "Descargas",
+  },
+  exp_academica: {
+    en: "ACADEMIC",
+    es: "ACADÉMICA",
+  },
+  exp_cursos: {
+    en: "COURSES",
+    es: "CURSOS",
+  },
+  exp_laboral: {
+    en: "WORK",
+    es: "LABORAL",
+  },
+  exp_tecnicatura: {
+    en: "Higher Technician Degree in Software Development",
+    es: "Tecnicatura Superior en Desarrollo de Software",
+  },
+  anio_pasantia: {
+    en: "2024 ~ Present",
+    es: "2024 ~ Presente",
+  },
+  anio_tecnicatura: {
+    en: "2024 ~ In progress",
+    es: "2024 ~ En curso",
+  },
+  anio_oracle: {
+    en: "2024 ~ In progress",
+    es: "2024 ~ En curso",
+  },
+  exp_freelance_title: {
+    en: "Freelance professional",
+    es: "Profesional independiente",
+  },
+  anio_freelance: {
+    en: "2018 ~ Present",
+    es: "2018 ~ Presente",
+  },
+  exp_universidad: {
+    en: "Bachelor in Visual Communication Design. University Degree. Facultad de Arquitectura, Diseño y Urbanismo.",
+    es: "Licenciatura en Diseño de Comunicación Visual. Carrera de grado. Facultad de Arquitectura, Diseño y Urbanismo.",
+  },
+  anio_universidad: {
+    en: "2005 ~ Pending thesis",
+    es: "2005 ~ Tesina pendiente",
+  },
+  exp_argentina: {
+    en: "■ #SeProgramar course (INTI)<br>■ Front-end Development (Ticmas)<br>■ Android App Development (Ticmas)<br>■ Front-end Development (Educado en Argentina)<br>■ English Level 8 B1.2 MCER (CUI-UBA)",
+    es: "■ Curso #SeProgramar (INTI)<br>■ Desarrollo Front-end (Ticmas)<br>■ Desarrollo de aplicaciones Android (Ticmas)<br>■ Desarrollo Front-end (Educado en Argentina)<br>■ Inglés Nivel 8 B1.2 MCER (CUI-UBA)",
+  },
+  exp_motion_title: {
+    en: "Postgraduate course FADU UNL",
+    es: "Curso de Posgrado FADU UNL",
+  },
+  exp_motion: {
+    en: "Motion Graphics Design: From graphic design to motion design.",
+    es: "Motion Graphics Design: Del diseño gráfico al diseño en movimiento.",
+  },
+  exp_pasantia: {
+    en: "Visual Communication Design Internship.",
+    es: "Pasantía de Diseño de Comunicación Visual.",
+  },
+  exp_fusa: {
+    en: "Freelance multimedia design.",
+    es: "Diseño multimedial independiente.",
+  },
+  exp_freelance: {
+    en: "Software development and multimedia design.",
+    es: "Desarrollo de software y diseño multimedial.",
+  },
+  exp_seal: {
+    en: "Administration and invoicing in the plastic products industry.",
+    es: "Administración y facturación en la industria de productos plásticos.",
+  },
+  exp_zona: {
+    en: "Sales and technical service of video games and computing.",
+    es: "Ventas y servicio técnico de videojuegos e informática.",
+  },
+  exp_oracle: {
+    en: "Training Program with Front-end Specialization.",
+    es: "Programa de formación con especialización Front-end",
+  },
+  exp_codoacodo: {
+    en: "■ React JS specialization course<br/>■ Soft Skills Course<br/>■ Full Stack Web Development - Node.JS",
+    es: "■ Curso de especialización React JS<br/>■ Curso de Habilidades Blandas<br/>■ Full Stack Desarrollo Web - Node.JS",
+  },
+  exp_pixi: {
+    en: "Game development with Pixi.JS",
+    es: "Desarrollo de videojuegos con Pixi.JS",
+  },
+  exp_react: {
+    en: "Application development with React and React Native",
+    es: "Desarrollo de aplicaciones con React y React Native",
+  },
+  hab_desarrollo: {
+    en: "DEVELOPMENT",
+    es: "DESARROLLO",
+  },
+  hab_diseño: {
+    en: "DESIGN",
+    es: "DISEÑO",
+  },
+  hab_musica: {
+    en: "OTHERS",
+    es: "OTRAS",
+  },
+  hab_identidadvisual: {
+    en: "Visual identity",
+    es: "Identidad visual",
+  },
+  hab_diseñowebyapps: {
+    en: "Web design & Apps",
+    es: "Diseño web y Apps",
+  },
+  hab_interfazdeusuario: {
+    en: "User interface",
+    es: "Interfaz de usuario",
+  },
+  hab_diseñodeinformacion: {
+    en: "Information design",
+    es: "Diseño de información",
+  },
+  hab_diseñoeditorial: {
+    en: "Editorial design",
+    es: "Diseño editorial",
+  },
+  hab_proactividad: {
+    en: "Proactivity",
+    es: "Proactividad",
+  },
+  hab_equipo: {
+    en: "Teamwork",
+    es: "Trabajo en equipo",
+  },
+  hab_comunicacion: {
+    en: "Effective communication",
+    es: "Comunicación efectiva",
+  },
+  hab_adaptabilidad: {
+    en: "Adaptability",
+    es: "Adaptabilidad",
+  },
+  hab_musicaoriginal: {
+    en: "Original music",
+    es: "Música original",
+  },
+  hab_efectosdesonido: {
+    en: "Sound effects",
+    es: "Efectos de sonido",
+  },
+  hab_vst: {
+    en: "VST Virtual instruments",
+    es: "Instrumentos virtuales VST",
+  },
+  proyecto_schokoladenbar: {
+    en: "Casual web video game",
+    es: "Videojuego web casual",
+  },
+  proyecto_pino: {
+    en: "Platform game",
+    es: "Videojuego de plataformas",
+  },
+  proyecto_rockquiz: {
+    en: "Music, puzzle & quiz game",
+    es: "Videojuego de música, puzzle y quiz",
+  },
+  proyecto_spacewars: {
+    en: "Shoot'em up game",
+    es: "Videojuego shoot'em up",
+  },
+  proyecto_mostrimaker: {
+    en: "Global Game Jam 2024 game",
+    es: "Juego para la Global Game Jam 2024",
+  },
+  proyecto_futumag: {
+    en: "Digital magazine about design and technology",
+    es: "Magazine digital sobre diseño y tecnología",
+  },
+  proyecto_wesound: {
+    en: "Magazine digital sobre diseño y tecnología",
+    es: "Social network for collab music production",
+  },
+  proyecto_pirogram: {
+    en: "Social network for collab music production",
+    es: "Identidad visual para estudio de diseño",
+  },
+  proyecto_seal: {
+    en: "Visual identity for design studio",
+    es: "Identidad, impresos y web",
+  },
+  proyecto_bienal: {
+    en: "Animation and original music",
+    es: "Animación y música original",
+  },
+  // proyecto_weather: {
+  //     en:"Website to check the weather",
+  //     es:"Sitio web para consultar el clima"
+  // },
+  masDesarrollo: {
+    en: "Development projects on GitHub",
+    es: "Proyectos de Desarrollo en GitHub",
+  },
+  masDiseño: {
+    en: "Design projects on Behance",
+    es: "Proyectos de Diseño en Behance",
+  },
+  certificates: {
+    en: "📄 Certificates PDF",
+    es: "📄 Certificados PDF",
+  },
+};
+
+export const elements_placeholder = {
+  form_nombre: {
+    en: "Name",
+    es: "Nombre",
+  },
+  form_mensaje: {
+    en: "Message",
+    es: "Mensaje",
+  },
+};
+
+export const elements_value = {
+  boton_enviar: {
+    en: "Send",
+    es: "Enviar",
+  },
+};
